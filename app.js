27670202d608b879960c5816075fc90200d12b38
nezrinn <nezrinhesenova607@gmail.com>
2023-11-15 23:36:10 +0400

    rose

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    const rubButton = document.getElementById('rubl');
-    const usdButton = document.getElementById('usd');
-    const input1 = document.querySelector('.inp1');
-    const input2 = document.querySelector('.inp2');
-    const cat1 = document.querySelector('.cat1');
-    const cat2 = document.querySelector('.cat2');
-
-    rubButton.addEventListener('click', function () {
-        cat1.textContent = 'RUB';
-        cat2.textContent = 'RUB';
-    });
-
-    usdButton.addEventListener('click', function () {
-        cat1.textContent = 'USD';
-        cat2.textContent = 'USD';
-    });
-
-    input1.addEventListener('input', function () {
-        convertCurrency(input1, cat1, input2, cat2);
-    });
-
-    input2.addEventListener('input', function () {
-        convertCurrency(input2, cat2, input1, cat1);
-    });
-
-    function convertCurrency(inputFrom, catFrom, inputTo, catTo) {
-        const amount = parseFloat(inputFrom.value);
-        if (catFrom.textContent === 'RUB' && catTo.textContent === 'USD') {
-            inputTo.value = (amount / 73.5).toFixed(2);
-        } else if (catFrom.textContent === 'USD' && catTo.textContent === 'RUB') {
-            inputTo.value = (amount * 73.5).toFixed(2);
-        }
-        
+document.addEventListener("DOMContentLoaded", function () {
+    let money1Buttons = document.querySelectorAll('.money1 button');
+    let money2Buttons = document.querySelectorAll('.money2 button');
+    let inp1 = document.querySelector('.inp1');
+    let inp2 = document.querySelector('.inp2');
+    let cat1 = document.querySelector('.cat1');
+    let cat2 = document.querySelector('.cat2');
+    function updateValues(amount, targetInput, targetCurrency) {
+        fetch(`https://v6.exchangerate-api.com/v6/584da95aee71216e9c39b42e/latest/${targetCurrency}`)
+            .then(response => response.json())
+            .then(data => {
+                const exchangeRate = data.conversion_rates["USD"];
+                const convertedAmount = amount * exchangeRate;
+                targetInput.value = convertedAmount.toFixed(2);
+                cat1.textContent = `1 ${targetCurrency} = ${exchangeRate.toFixed(4)} USD`;
+                cat2.textContent = `1 USD = ${(1 / exchangeRate).toFixed(4)} ${targetCurrency}`;
+            })
+            .catch(error => console.error('Error fetching exchange rates:', error));
     }
-})
+    money1Buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            money1Buttons.forEach(function (btn) {
+                btn.classList.remove('active');
+            });
+            button.classList.add('active');
+            updateValues(inp1.value, inp2, button.innerText);
+        });
+    });
+    money2Buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            money2Buttons.forEach(function (btn) {
+                btn.classList.remove('active');
+            });
+            button.classList.add('active');
+            updateValues(inp2.value, inp1, button.innerText);
+        });
+    });
+    inp1.addEventListener("input", function () {
+        this.value = this.value.replace(/[^0-9.]/g, '');
+        if (this.value.split('.').length > 2) {
+            this.value = this.value.slice(0, this.value.lastIndexOf('.'));
+        }
+        updateValues(parseFloat(this.value), inp2, document.querySelector('.money1 button.active').innerText);
+    });
+    inp2.addEventListener("input", function () {
+        this.value = this.value.replace(/[^0-9.]/g, '');
+        if (this.value.split('.').length > 2) {
+            this.value = this.value.slice(0, this.value.lastIndexOf('.'));
+        }
+        updateValues(parseFloat(this.value), inp1, document.querySelector('.money2 button.active').innerText);
+    });
+});
